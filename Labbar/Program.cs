c7681652c4a92df3009a5695383955ac5397ac09
JomamaJr <JomamaJr@DESKTOP-1S3SKJJ>
2023-11-14 13:04:51 +0100

    Add project files.

---
+++ Labbar/Program.cs
@@ -0,0 +1,62 @@
+﻿
+// Labb 1
+/*
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+Console.WriteLine("Hej Världen!");
+    
+    Console.ReadLine();
+*/
+
+// Labb 2
+/*
+string mittNamn = "Hampus";
+int minÅlder = 20;
+
+Console.WriteLine(mittNamn);
+Console.WriteLine("Din ålder är: " + minÅlder);
+Console.ReadLine();
+
+Console.WriteLine(mittNamn);
+Console.WriteLine("Din nya ålder är: " + (minÅlder + 1) );
+Console.ReadLine();
+
+*/
+
+// Labb 3
+/*
+Console.Write("Skriv Ditt namn: ");
+string returvärde = Console.ReadLine();
+Console.WriteLine("Hej på dig " + returvärde);
+Console.Write("Vilken är din favorit färg: ");
+string returvärde2 = Console.ReadLine();
+Console.WriteLine(returvärde2 + " Wow det är min favorit med");
+Console.ReadLine();
+*/
+
+// Labb 4 
+/*
+ Console.Write("Lösenord: ");
+string Lösenord = Console.ReadLine()!;
+
+if (Lösenord == "1234")
+{ 
+ Console.WriteLine("Välkommen till systemet");
+}
+else
+{ 
+Console.WriteLine("Fel lösenord"); 
+
+Console.ReadLine();
+}
+*/
+
